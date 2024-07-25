2eeccff5e1000e0abc70e0514303dc34ca63d3bd
Sunny-12v <sunnykumar280260@gmail.com>
2024-07-25 21:06:33 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <HeadlineCards/>
+    <Food/>
+    <Category/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
